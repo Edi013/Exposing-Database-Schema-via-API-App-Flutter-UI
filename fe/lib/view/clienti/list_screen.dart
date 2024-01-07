b3247bbf1a07328514503b390a4e047ae8220b04
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2024-01-07 11:13:13 +0200

    State management is specified in main file, so it is being used bad here.
    void main() {
      try {
        runApp(
          MultiProvider(
            providers: [
              ChangeNotifierProvider<ComenziProvider>(
                  create: (_) => ComenziProvider()),
              ChangeNotifierProvider<ClientiProvider>(
                  create: (_) => ClientiProvider()),
            ],
            child: const MyApp(),
          ),
        );
      } catch (e, stackTrace) {
        print("Error: $e + Stacktrace: $stackTrace");
      }
    }

--- fe/lib/view/clienti/list_screen.dart
+++ fe/lib/view/clienti/list_screen.dart
@@ -11,78 +11,75 @@ class ListClientiScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ClientiProvider(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Clienti List'),
-        ),
-        body: Container(
-          padding: const EdgeInsets.all(16.0),
-          child: Card(
-            elevation: 5.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            color: Theme.of(context).primaryColor,
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    'Clienti',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Clienti List'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          color: Theme.of(context).primaryColor,
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Clienti',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Expanded(
-                  child: Consumer<ClientiProvider>(
-                    builder: (context, clientiProvider, child) {
-                      return ListView.builder(
-                        itemCount: clientiProvider.clientiList.length,
-                        itemBuilder: (context, index) {
-                          Clienti clienti = clientiProvider.clientiList[index];
-                          return ListTile(
-                            title: Text('ID: ${clienti.id}'),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Nume: ${clienti.nume}'),
-                                Text('Prenume: ${clienti.prenume}'),
-                                Text('Adresa: ${clienti.adresa}'),
-                                Text('Companie: ${clienti.companie}'),
-                                Text('Oras: ${clienti.oras}'),
-                                Text('Cod postal: ${clienti.cod}'),
-                                Text('Telefon: ${clienti.telefon}'),
-                                IconButton(
-                                  onPressed: () => listClientiController
-                                      .deleteById(context, clienti.id!),
-                                  icon: const Icon(
-                                    Icons.delete,
-                                    color: Colors.red,
-                                  ),
+              ),
+              Expanded(
+                child: Consumer<ClientiProvider>(
+                  builder: (context, clientiProvider, child) {
+                    return ListView.builder(
+                      itemCount: clientiProvider.clientiList.length,
+                      itemBuilder: (context, index) {
+                        Clienti clienti = clientiProvider.clientiList[index];
+                        return ListTile(
+                          title: Text('ID: ${clienti.id}'),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Nume: ${clienti.nume}'),
+                              Text('Prenume: ${clienti.prenume}'),
+                              Text('Adresa: ${clienti.adresa}'),
+                              Text('Companie: ${clienti.companie}'),
+                              Text('Oras: ${clienti.oras}'),
+                              Text('Cod postal: ${clienti.cod}'),
+                              Text('Telefon: ${clienti.telefon}'),
+                              IconButton(
+                                onPressed: () => listClientiController
+                                    .deleteById(context, clienti.id!),
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
                                 ),
-                              ],
-                            ),
-                            onTap: () => listClientiController.updateItem(
-                                context, index, clienti),
-                          );
-                        },
-                      );
-                    },
-                  ),
+                              ),
+                            ],
+                          ),
+                          onTap: () => listClientiController.updateItem(
+                              context, index, clienti),
+                        );
+                      },
+                    );
+                  },
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ElevatedButton(
-                    onPressed: () => listClientiController.addItem(context),
-                    child: const Text('Add Clienti'),
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: () => listClientiController.addItem(context),
+                  child: const Text('Add Clienti'),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- fe/lib/view/comenzi/list_screen.dart
+++ fe/lib/view/comenzi/list_screen.dart
@@ -11,75 +11,72 @@ class ListComenziScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ComenziProvider(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Comenzi List'),
-        ),
-        body: Container(
-          padding: const EdgeInsets.all(16.0),
-          child: Card(
-            elevation: 5.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            color: Theme.of(context).primaryColor,
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    'Comenzi',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Comenzi List'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          color: Theme.of(context).primaryColor,
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Comenzi',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Expanded(
-                  child: Consumer<ComenziProvider>(
-                    builder: (context, comenziProvider, child) {
-                      return ListView.builder(
-                        itemCount: comenziProvider.comenziList.length,
-                        itemBuilder: (context, index) {
-                          Comenzi comenzi = comenziProvider.comenziList[index];
-                          return ListTile(
-                            title: Text('ID: ${comenzi.id}'),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('ID Clienti: ${comenzi.idClienti}'),
-                                Text('Data Plasare: ${comenzi.dataPlasare}'),
-                                Text('Data Onorare: ${comenzi.dataOnorare}'),
-                                Text('Data Plata: ${comenzi.dataPlata}'),
-                                IconButton(
-                                  onPressed: () => listComenziController
-                                      .deleteById(context, comenzi.id!),
-                                  icon: const Icon(
-                                    Icons.delete,
-                                    color: Colors.red,
-                                  ),
+              ),
+              Expanded(
+                child: Consumer<ComenziProvider>(
+                  builder: (context, comenziProvider, child) {
+                    return ListView.builder(
+                      itemCount: comenziProvider.comenziList.length,
+                      itemBuilder: (context, index) {
+                        Comenzi comenzi = comenziProvider.comenziList[index];
+                        return ListTile(
+                          title: Text('ID: ${comenzi.id}'),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('ID Clienti: ${comenzi.idClienti}'),
+                              Text('Data Plasare: ${comenzi.dataPlasare}'),
+                              Text('Data Onorare: ${comenzi.dataOnorare}'),
+                              Text('Data Plata: ${comenzi.dataPlata}'),
+                              IconButton(
+                                onPressed: () => listComenziController
+                                    .deleteById(context, comenzi.id!),
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
                                 ),
-                              ],
-                            ),
-                            onTap: () => listComenziController.updateItem(
-                                context, index, comenzi),
-                          );
-                        },
-                      );
-                    },
-                  ),
+                              ),
+                            ],
+                          ),
+                          onTap: () => listComenziController.updateItem(
+                              context, index, comenzi),
+                        );
+                      },
+                    );
+                  },
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ElevatedButton(
-                    onPressed: () => listComenziController.addItem(context),
-                    child: const Text('Add Comenzi'),
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: () => listComenziController.addItem(context),
+                  child: const Text('Add Comenzi'),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
